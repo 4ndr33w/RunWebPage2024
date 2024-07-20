6e9c336c9ab65942327e0593ac143d7a6e9694b1
Andrew Muhlynin <rx.081@hotmail.com>
2024-07-20 22:11:55 +0500

    Загружается таблица участников

--- RunWebPage2024/Controllers/HomeController.cs
+++ RunWebPage2024/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using RunBot2024.DbContexts;
 using RunWebPage2024.Models;
 using System.Diagnostics;
 
@@ -7,26 +9,61 @@ namespace RunWebPage2024.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly NpgDbContext _pgContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        private List<RivalModel> _rivalsFilteredCollection;
+        private List<RivalModel> _rivalsFullCollection;
+
+        private List<FullCompanyList> _fullCompanyList;
+        
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, NpgDbContext npgDbContext)
         {
             _logger = logger;
+            _configuration = configuration;
+            _pgContext = npgDbContext;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            try
+            {
+                _rivalsFullCollection = _pgContext.Rivals
+                    .Where(x => x.TotalResult > 0)
+                    .OrderByDescending(x => x.TotalResult)
+                    .ToList();
+                
+            }
+            catch (Exception)
+            {
+                _rivalsFullCollection = new List<RivalModel>();
+                _fullCompanyList = new List<FullCompanyList>();
+            }
         }
 
         public IActionResult Index()
         {
-            return View();
+            Tuple<List<RivalModel>, string, string> compositeModel;
+            string gender = "male";
+            string company = "all";
+
+            _rivalsFilteredCollection = _rivalsFullCollection;
+            compositeModel = new Tuple<List<RivalModel>, string, string>(_rivalsFullCollection, company, gender);
+            return View(compositeModel);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
---
+++ RunWebPage2024/Models/FullCompanyList.cs
@@ -0,0 +1,21 @@
+﻿namespace RunWebPage2024.Models
+{
+    public class FullCompanyList
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string CityName { get; set; }
+        public string RegionName { get; set; }
+
+        public FullCompanyList()
+        { }
+
+        public FullCompanyList(FullCompanyList companyListItem)
+        {
+            //Id = companyListItem.Id;
+            CompanyName = companyListItem.CompanyName;
+            CityName = companyListItem.CityName;
+            RegionName = companyListItem.RegionName;
+        }
+    }
+}
